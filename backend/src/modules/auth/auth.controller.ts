bda1a296a6bc752caefd455d78dc13199bec8c30
Hrishikesh Pathak <hrishikeshpathak1009@gmail.com>
2026-07-10 00:40:40 +0530

    logout feat added

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -86,5 +86,8 @@ export const logoutController = async (
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   path: "/",
-});
+}); return res.status(200).json({
+    success: true,
+    message: "Logged out successfully",
+  });
 };
